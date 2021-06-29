1d25788880e84234340350e906241c306025c071
sejin <sjhan@ematsoft.co.kr>
2021-06-29 19:24:18 +0900

    still working 1436

--- level_11/1436_python.py
+++ level_11/1436_python.py
@@ -4,22 +4,70 @@ import sys
 preStr = '0'
 postStr = ''
 
+def stringAdder(a, b):
+    lenA = len(a)
+    lenB = len(b)
+    adder = 0
+
+    reverseStringA = a[::-1]
+    reverseStringB = b[::-1]
+    sumString = ''
+
+    for i in range(min(lenA,lenB)):
+        tmpSum = int(reverseStringA[0])+int(reverseStringB[0])+adder
+        sumString = str(tmpSum%10)+sumString
+        adder = tmpSum//10
+        reverseStringA[0] = ''
+        reverseStringB[0] = ''
+
+
+    return adder, sumString
+
+
+
+currentNum = '666'
+
+def getNext(currentNo:str):
+
+    pass
+
+
+
 #comment(sj:2021-06-29): check the post number and set a post number condition.
 
-def getNextNum():
-    middle = '666'
-    preNum = int(preStr)
-    if preNum%10 != 5 and preNum%10 != 6:
-        preStr = str(int(preStr)+1)
+def getNextNum( present: str):
+    numSplit = present.split('666',maxsplit=1)
+    
+    if len(numSplit[1]) > 0:
+        if str(10**len(numSplit[1])-1) != numSplit[1]:
+            numSplit[1] = ("{0"+str(len(numSplit[1]))+"d}").format(int(numSplit[1])+1)
+        else:
+            numSplit[1] = ''
+            numSplit[0] = str(int(numSplit[0]+'6'*min(3,len(numSplit[1])))+1)+'0'*max(0,len(numSplit[1])-3)
     else:
-        if preNum%10 == 5:
-            preNum += 1
-            middle = '66'
-            postStr = '0'*len(postStr)
-        elif preNum%10 == 6:
-            if int(postStr) != (10**(len(str(postStr))-1)):
-                postStr = str(int(postStr)+1)
-            else:
-                
+        numSplit[0] = str(int(numSplit[0])+1)
+        if '666' in numSplit[0]:
+            numSplit2 = numSplit[0].split('666',maxsplit=1)
+            numSplit[0] = numSplit2[0]
+            numSplit[1] = numSplit2[1]+'000'
+        else:
+            count6 = 0
+            for i in numSplit[0][::-1]:
+                if numSplit[0][i] == '6':
+                    count6 +=1
+                else:
+                    break
+            
 
-            middle = '66'
+
+
+
+
+
+testStr = '1666'
+
+aa  = testStr.split('666')
+
+print(len(aa))
+print(aa[0])
+print(aa[1])
