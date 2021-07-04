4b3626fa019e8d908cfc68393496d07b052f9f79
james_mac <dm7exir@gmail.com>
2021-07-04 17:46:32 +0900

    1436 complete

--- level_11/1436_python.py
+++ level_11/1436_python.py
@@ -1,49 +1,14 @@
 import sys
 
-
-preStr = '0'
-postStr = ''
-
-def stringAdder(a, b):
-    lenA = len(a)
-    lenB = len(b)
-    adder = 0
-
-    reverseStringA = a[::-1]
-    reverseStringB = b[::-1]
-    sumString = ''
-
-    for i in range(min(lenA,lenB)):
-        tmpSum = int(reverseStringA[0])+int(reverseStringB[0])+adder
-        sumString = str(tmpSum%10)+sumString
-        adder = tmpSum//10
-        reverseStringA[0] = ''
-        reverseStringB[0] = ''
-
-
-    return adder, sumString
-
-
-
-currentNum = '666'
-
-def getNext(currentNo:str):
-
-    pass
-
-
-
-#comment(sj:2021-06-29): check the post number and set a post number condition.
-
 def getNextNum( present: str):
     numSplit = present.split('666',maxsplit=1)
     
     if len(numSplit[1]) > 0:
         if str(10**len(numSplit[1])-1) != numSplit[1]:
-            numSplit[1] = ("{0"+str(len(numSplit[1]))+"d}").format(int(numSplit[1])+1)
+            numSplit[1] = ("{:0"+str(len(numSplit[1]))+"d}").format(int(numSplit[1])+1)
         else:
-            numSplit[1] = ''
             numSplit[0] = str(int(numSplit[0]+'6'*min(3,len(numSplit[1])))+1)+'0'*max(0,len(numSplit[1])-3)
+            numSplit[1] = ''
     else:
         numSplit[0] = str(int(numSplit[0])+1)
         if '666' in numSplit[0]:
@@ -53,21 +18,34 @@ def getNextNum( present: str):
         else:
             count6 = 0
             for i in numSplit[0][::-1]:
-                if numSplit[0][i] == '6':
+                if i == '6':
                     count6 +=1
                 else:
                     break
-            
+                           
+            numSplit[1] = numSplit[1]+('0'*count6)
+            numSplit[0] = numSplit[0][:(len(numSplit[0])-count6)]
+        
+    return numSplit[0]+'666'+numSplit[1]
+
+usrIn = int(sys.stdin.readline().rstrip())
+
+# resultArray = ['','666','1666']+['']*9998
+# for i in range(3,10001):
+#     resultArray[i] = getNextNum(resultArray[i-1])
 
 
+# print(resultArray[usrIn])
 
 
+if usrIn == 1:
+    print('666')
+elif usrIn == 2:
+    print('1666')
+else:
+    nextVersion = '1666'
+    for _ in range(usrIn-2):
+        nextVersion = getNextNum(nextVersion)
+    print(nextVersion)
 
 
-testStr = '1666'
-
-aa  = testStr.split('666')
-
-print(len(aa))
-print(aa[0])
-print(aa[1])
